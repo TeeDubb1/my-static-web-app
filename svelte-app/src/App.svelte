41e511193e952a74d92dfe39202a59b9eccd484d
Tom Wright <TEWright@tql.com>
2022-07-15 13:22:55 -0400

    delete unnecessary files

--- svelte-app/src/App.svelte
+++ svelte-app/src/App.svelte
@@ -1,26 +1,24 @@
 <script>
   import './styles.scss';
   import { Router, Link, Route } from 'svelte-routing';
-  import About from './About.svelte';
-  import Products from './products/Products.svelte';
   import MFA from './authenticated/MFA.svelte';
 
-  import { HeaderBar, NavBar, PageNotFound, Redirect } from './components';
+  import { PageNotFound, Redirect } from './components';
 
   export let url = '';
 </script>
 
-<HeaderBar />
-<div class="section columns">
+<!-- <HeaderBar /> -->
+<!-- <div class="section columns"> -->
   <Router {url}>
-    <NavBar />
+    <!-- <NavBar /> -->
     <main class="column">
       <div>
-        <!-- <Route path="/">
+        <Route path="/">
           <Redirect path="/authenticated/MFA" />
-        </Route> -->
+        </Route>
         <Route path="/authenticated/MFA" component={MFA} />
       </div>
     </main>
   </Router>
-</div>
+<!-- </div> -->
